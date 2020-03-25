29b72b3321a46d4768171da5e09ca9c63e81beb1
abdo ramzy <abdo.ramzy511@gmail.com>
2020-03-25 02:05:52 +0200

    Initial Commit

---
+++ Youtube Video Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube Video Player
+//
+//  Created by Abdo on 3/25/20.
+//  Copyright © 2020 abdo ramzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Youtube Video PlayerTests/Youtube_Video_PlayerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Youtube_Video_PlayerTests.swift
+//  Youtube Video PlayerTests
+//
+//  Created by Abdo on 3/25/20.
+//  Copyright © 2020 abdo ramzy. All rights reserved.
+//
+
+import XCTest
+@testable import Youtube_Video_Player
+
+class Youtube_Video_PlayerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
